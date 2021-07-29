a0acc9e6f6a2b85ecffcc3411a4465a9437bcbbb
jyotirm0y-paul <jyotirmoooy@gmail.com>
2021-07-29 20:57:45 +0600

    add

---
+++ src/Components/Home/Admin/AddAdmin/AddAdmin.js
@@ -0,0 +1,53 @@
+import React, { useContext, useState } from 'react';
+import { UserContext } from '../../../../App';
+// import { UserContext } from '../../../App';
+import Sidebar from '../Sidebar/Sidebar';
+import Sidebar2 from '../Sidebar2/Sidebar2';
+
+const AddAdmin = () => {
+  const [loggedInUser, setLoggedInUser] =useContext(UserContext)
+    const [info, setInfo] = useState({});
+    const handleBlur = e => {
+        const newInfo = { ...info };
+        newInfo[e.target.name] = e.target.value;
+        setInfo(newInfo);
+    }
+
+    const handleSubmit = () => {
+        const formData = new FormData()
+        console.log(info);
+        formData.append('email', info.email);
+
+        fetch('https://enigmatic-mountain-35188.herokuapp.com/addAdmin', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
+
+    return (
+        <section className="container-fluid row">
+            <Sidebar2></Sidebar2>
+            <div className="col-md-10 p-4 pr-5" style={{ position: "absolute", right: 0, backgroundColor: "#F4FDFB" }}>
+                <h5 className="text-brand">Add a Service</h5>
+                <form onSubmit={handleSubmit}>
+                    
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Please Add a Admin</label>
+                        <input onBlur={handleBlur} type="text" className="form-control" name="email" placeholder="Add Admin" />
+                    </div>
+                    
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </section>
+    );
+};
+
+export default AddAdmin;
